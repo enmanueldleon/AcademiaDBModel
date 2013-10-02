d11c971bb5deebdb38452225b6852e7a5de1456f
enmanueldleon <enmanueldleon@gmail.com>
2013-10-01 23:49:49 -0400

    DbModel

--- AcademiaDBModel/ModeloAcademiaDB.Designer.cs
+++ AcademiaDBModel/ModeloAcademiaDB.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Default code generation is disabled for model 'C:\Users\Enmanuel\Documents\Visual Studio 2012\Projects\AcademiaDBModel\AcademiaDBModel\ModeloAcademiaDB.edmx'. 
+﻿// Default code generation is disabled for model 'C:\Users\Enmanuel\documents\visual studio 2012\Projects\AcademiaDBModel\AcademiaDBModel\ModeloAcademiaDB.edmx'. 
 // To enable default code generation, change the value of the 'Code Generation Strategy' designer
 // property to an alternate value. This property is available in the Properties Window when the model is
 // open in the designer.
